706b4966e5a6ead6a2c010d2e5fb92d33966956f
Jamus24 <jamesfg24@icloud.com>
2026-08-15 23:44:22 +0200

    fix: add serverExternalPackages for libsql

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "standalone",
-  /* config options here */
+  serverExternalPackages: ['@prisma/adapter-libsql', '@libsql/client'],
   typescript: {
     ignoreBuildErrors: true,
   },
